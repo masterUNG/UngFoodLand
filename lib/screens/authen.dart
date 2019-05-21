ce695c427bcd68d327cd1b9ca78a87f478a41df5
masterUNG <phrombutr@gmail.com>
2019-05-21 14:49:46 +0700

    Create Title and Logo Success

---
+++ lib/screens/authen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Authen extends StatefulWidget {
+  @override
+  _AuthenState createState() => _AuthenState();
+}
+
+class _AuthenState extends State<Authen> {
+  Widget showLogo() {
+    return Image.asset('images/logo.png');
+  }
+
+  Widget showTitle() {
+    return Text(
+      'Ung Food Land',
+      style: TextStyle(
+          fontSize: 25.0, fontWeight: FontWeight.bold, color: Colors.red[900]),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      margin: EdgeInsets.only(top: 70.0),
+      alignment: Alignment(0, -1),
+      child: Column(
+        children: <Widget>[
+          showLogo(),
+          Container(margin: EdgeInsets.only(top: 10.0),
+            child: showTitle(),
+          )
+        ],
+      ),
+    ));
+  }
+}
