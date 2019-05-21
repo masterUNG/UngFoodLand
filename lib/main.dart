dd6801d154291166f0cbeee01bae6031cb8b607d
masterUNG <phrombutr@gmail.com>
2019-05-21 16:36:13 +0700

    finish 21May

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ung_food_land/screens/authen.dart';
 
-
-
-void main(){
+void main() {
   runApp(MyApp());
 }
-
-
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return MaterialApp(home: Authen());
   }
 }
