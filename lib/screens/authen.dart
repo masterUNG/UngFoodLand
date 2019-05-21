55f744a486fdfd67dfa1f52e7f9fdc7d558277a5
masterUNG <phrombutr@gmail.com>
2019-05-21 16:05:46 +0700

    update

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,6 +6,20 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget showSignUp() {
+    return RaisedButton(
+      child: Text('Sign Up'),
+      onPressed: () {},
+    );
+  }
+
+  Widget showSignIn() {
+    return RaisedButton(
+      child: Text('Sign In'),
+      onPressed: () {},
+    );
+  }
+
   Widget showPassword() {
     return TextFormField(
       decoration: InputDecoration(
@@ -48,8 +62,22 @@ class _AuthenState extends State<Authen> {
             margin: EdgeInsets.only(left: 50.0, right: 50.0),
             child: showUser(),
           ),
-          Container(margin: EdgeInsets.only(left: 50.0, right: 50.0),
+          Container(
+            margin: EdgeInsets.only(left: 50.0, right: 50.0),
             child: showPassword(),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 50.0, right: 50.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: showSignIn(),
+                ),
+                Expanded(
+                  child: showSignUp(),
+                )
+              ],
+            ),
           )
         ],
       ),
