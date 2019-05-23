628b3afe6f55d4809d79cc886ada8bdb72476408
masterUNG <phrombutr@gmail.com>
2019-05-23 11:00:17 +0700

    update

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -6,12 +6,57 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final formKey = GlobalKey<FormState>();
+
+  Widget registerButton() {
+    return IconButton(
+      icon: Icon(Icons.cloud_upload),
+      onPressed: () {
+        print('You Click Register');
+      },
+    );
+  }
+
+  Widget nameTextFromField() {
+    return TextFormField(
+      decoration:
+          InputDecoration(labelText: 'Name :', hintText: 'Type Your Name'),
+    );
+  }
+
+  Widget userTextFromField() {
+    return TextFormField(
+      decoration:
+          InputDecoration(labelText: 'User :', hintText: 'Type Your User'),
+    );
+  }
+
+  Widget passwordTextFromField() {
+    return TextFormField(
+      decoration:
+          InputDecoration(labelText: 'Password :', hintText: 'More 6 Charator'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Register'),
-      ),body: Text('This is body'),
+        actions: <Widget>[registerButton()],
+      ),
+      body: Form(
+        child: Container(
+          padding: EdgeInsets.all(50.0),
+          child: Column(
+            children: <Widget>[
+              nameTextFromField(),
+              userTextFromField(),
+              passwordTextFromField()
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
